727a0466902371bd5a6ac812a484ea99cd2c962b
Sydney Mercier <sydney.mercier91@gmail.com>
2018-06-20 15:59:49 -0400

    spelling errors are the bane of my existence

--- src/main/java/com/revature/servlets/IndexServlet.java
+++ src/main/java/com/revature/servlets/IndexServlet.java
@@ -15,7 +15,7 @@ public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("Index.html").forward(request, response);
+		request.getRequestDispatcher("index.html").forward(request, response);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
